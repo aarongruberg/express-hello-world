07e61453755869afad354f12d98bc60729757d71
Aaron Gruberg <aarongruberg@Aarons-MacBook-Pro.local>
2021-08-13 08:47:52 -0700

    logging server side header data to the console

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.send("Hello there!"));
+// replacing this function
+//app.get("/", (req, res) => res.send("Hello there!"));
+
+// with this one to log header data to the console
+app.get("/", (req, res) => {
+    console.log(JSON.stringify(req.headers));
+    res.send("Hello from Render!");
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
